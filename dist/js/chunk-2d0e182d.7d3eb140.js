373b020d6bba18e49be01b0df742dc29dc912f8f
filigreti <aolfiligre@gmail.com>
2020-09-22 18:10:21 +0100

    WIp

---
+++ dist/js/chunk-2d0e182d.7d3eb140.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e182d"],{"7b6b":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("main",[t("router-view")],1)},r=[],c={},l=c,a=t("2877"),i=Object(a["a"])(l,u,r,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0e182d.7d3eb140.js.map
---
+++ dist/js/chunk-2d212c60.b26864e0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d212c60"],{aa9c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},u=[],r={},a=r,l=t("2877"),o=Object(l["a"])(a,c,u,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d212c60.b26864e0.js.map
